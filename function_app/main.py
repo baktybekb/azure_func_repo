637c0a0b714fa898edc9dd5fb35c9462669f399c
baktybek_baiserkeev <bakbbz61@gmail.com>
2024-06-05 16:15:38 +0600

    message

--- function_app/main.py
+++ function_app/main.py
@@ -6,9 +6,8 @@ import azure.functions as func
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).isoformat()
     logging.info(f'Timer triggered function ran at {utc_timestamp}')
-    logging.info('_' * 200)
-    print('=' * 200)
-    print('BAHAAAAAAA')
+    logging.info('=' * 100)
+    logging.info('BAHAAAAAAA main.py file')
 
 # import logging
 # import datetime
